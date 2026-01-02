cafdfb70be0250e78f88efb6f9292227ae72d0e1
UnknownCoderMaster <orzibeksalohiddinov@gmail.com>
2026-01-02 16:22:05 +0500

    Refactor RegisterCommandValidator to simplify password validation rules

--- GameGo.Application/Features/Authentication/Commands/Register/RegisterCommandValidator.cs
+++ GameGo.Application/Features/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -17,11 +17,11 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
 			.WithMessage("Email format noto'g'ri");
 
 		RuleFor(x => x.Password)
-			.NotEmpty().WithMessage("Password is required")
-			.MinimumLength(8).WithMessage("Password must be at least 8 characters")
-			.Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
-			.Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
-			.Matches(@"[0-9]").WithMessage("Password must contain at least one number");
+			.NotEmpty().WithMessage("Password is required");
+			// .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+			// .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+			// .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+			// .Matches(@"[0-9]").WithMessage("Password must contain at least one number");
 
 		RuleFor(x => x.PhoneNumber)
 			.NotEmpty().WithMessage("Phone number is required")
